64a9f2c343892fcc9922c1e4333bf3c38dac8f02
Sam <lunar_sam@protonmail.com>
2026-06-17 19:46:26 -0400

    build tools core script

---
+++ tools/build_tools.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env lua
+
+local function panic(err)
+	io.stderr:write("error: ",err,"\n")
+	os.exit(1)
+end
+
+if not os.execute "[[ -e tgstation.dme ]]" then
+	panic("Script must be run from repo root.")
+end
+
+os.execute("mkdir -p buildtmp")
+
+local scripts = {}
+for line in io.popen("ls tools/linux_build_tools", "r"):lines() do
+	table.insert(scripts, line)
+end
+
+table.sort(scripts)
+
+for i=1, #scripts do
+	print("\27[1m:: "..scripts[i].."\27[0m")
+	dofile("tools/linux_build_tools/"..scripts[i])
+end
